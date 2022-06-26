052cceaaf1f24f64ac5e4bce50e84d9395ce647c
Roshan-Muhmd <roshanmuhmdnavas@gmial,com>
2022-06-26 16:20:07 +0530

    latest update, domain hosted

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,48 +2,78 @@ import classes from "./Container.module.css";
 import Tiles from "./UI/Tiles";
 import { FiSmile, FiLayers, FiPhone, FiMoreHorizontal } from "react-icons/fi";
 import Overlay from "./UI/Overlay";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
-
+const CONTENT_DATA = {
+  about: "hi my name is roshan",
+  project: "lot of projects to be listed",
+  contact: "roshanmuhmdnavas@gmail.com",
+  more: "lots of more to be updated",
+};
 
 const Container = () => {
   const [overlayIsActive, setOverlayIsActive] = useState(false);
+  const [overlayMessage, setOverlayMessage] = useState("");
 
-  const overlayHandler = () => {
+  const overlayHandler = (message) => {
     setOverlayIsActive(!overlayIsActive);
+    setOverlayMessage(message);
   };
 
-  const onCancelHandler =() => {
-    setOverlayIsActive(false)
-  }
+  const onCancelHandler = () => {
+    setOverlayIsActive(false);
+  };
 
   return (
-    <div className={classes.content}>
-      <Tiles onClick={overlayHandler}  >
-        <div className={classes.position}>
-          <FiSmile className={classes.icon}></FiSmile>About me
-        </div>
-       
-      </Tiles>
-      {overlayIsActive && <Overlay  onCancel={onCancelHandler} />}
-      <Tiles>
-        <div className={classes.position}>
-          <FiLayers className={classes.icon}></FiLayers>My Projects
-        </div>
-      </Tiles>
-    
-      <Tiles>
-        <div className={classes.position}>
-          <FiPhone className={classes.icon}></FiPhone>
-          <div>Contact</div>
-        </div>
-      </Tiles>
-      <Tiles>
-        <div className={classes.position}>
-          <FiMoreHorizontal className={classes.icon}></FiMoreHorizontal>More
-        </div>
-      </Tiles>
-    </div>
+    <Fragment>
+      {overlayIsActive && (
+        <Overlay onCancel={onCancelHandler} about={overlayMessage} />
+      )}
+      <div className={classes.content}>
+        <Tiles
+          onClick={() => {
+            overlayHandler(CONTENT_DATA.about);
+          }}
+        >
+          <div className={classes.position}>
+            <FiSmile className={classes.icon}></FiSmile>About me
+          </div>
+        </Tiles>
+
+        <Tiles
+          about="lots of projects"
+          onClick={() => {
+            overlayHandler(CONTENT_DATA.project);
+          }}
+        >
+          <div className={classes.position}>
+            <FiLayers className={classes.icon}></FiLayers>My Projects
+          </div>
+        </Tiles>
+
+        <Tiles
+          about="call me"
+          onClick={() => {
+            overlayHandler(CONTENT_DATA.contact);
+          }}
+        >
+          <div className={classes.position}>
+            <FiPhone className={classes.icon}></FiPhone>
+            <div>Contact</div>
+          </div>
+        </Tiles>
+        <Tiles
+          about="nothing more"
+          onClick={() => {
+            overlayHandler(CONTENT_DATA.more);
+          }}
+        >
+          <div className={classes.position}>
+            <FiMoreHorizontal className={classes.icon}></FiMoreHorizontal>More
+          </div>
+        </Tiles>
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/UI/Overlay.js
+++ src/components/UI/Overlay.js
@@ -1,11 +1,15 @@
 import React from "react";
 import classes from "./Overlay.module.css";
 
+
+
 const Overlay = (props) => {
+
+
   return (
       <React.Fragment>
     <div onClick={props.onCancel} className={classes.backdrop}/>
-      <div className={classes.overlay}>data</div>
+      <div className={classes.overlay}>{props.about}</div>
       </React.Fragment>
   );
 };
